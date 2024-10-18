9a92f3a834f92a39507ea5e170519b7c1bdae5cd
Aertic <63727339+Aertic@users.noreply.github.com>
2024-10-18 22:59:22 +0200

    Make nav use usePathname; fix params; fix capitalization

--- components/nav.jsx
+++ components/nav.jsx
@@ -4,112 +4,114 @@ import { useEffect, useState } from 'react';
 import styles from '@/src/css/docs/nav.module.scss';
 import { useTheme } from './themeProvider';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-export default function Nav({ params }) {
-	const [docs, setDocs] = useState(null);
-	const { theme, setTheme } = useTheme();
-	const [open, setOpen] = useState([]);
-	const [sortedDocs, setSortedDocs] = useState({});
-	const [activeDoc, setActiveDoc] = useState('');
+export default function Nav({ project }) {
+    const params = usePathname();
+    const { theme, setTheme } = useTheme();
+    const [open, setOpen] = useState([]);
+    const [sortedDocs, setSortedDocs] = useState({});
+    const [activeDoc, setActiveDoc] = useState(params.slice(1));
 
-	useEffect(() => {
-		setActiveDoc(params.docs.join('/'));
-		const fetchDocs = async () => {
-			const res = await findDocs(params.docs[0]);
-			if (res == 'none') window.location.href = '/';
-			const parsedDocs = await JSON.parse(res);
-			setDocs(parsedDocs);
+    useEffect(() => {
+        setActiveDoc(params.slice(1));
+    }, [params]);
 
-			function sortDocs(docs) {
-				return docs.reduce((acc, doc) => {
-					const pathParts = doc.path.split('/');
-					const category = pathParts[1] || 'Uncategorized';
-					if (!acc[category]) {
-						acc[category] = [];
-					}
-					acc[category].push(doc);
-					return acc;
-				}, {});
-			}
-			setSortedDocs(sortDocs(parsedDocs));
-		};
+    useEffect(() => {
+        const fetchDocs = async () => {
+            const res = await findDocs(project + '/');
+            if (res == 'none') window.location.href = '/';
+            const parsedDocs = await JSON.parse(res);
 
-		fetchDocs();
-	}, [theme]);
+            function sortDocs(docs) {
+                return docs.reduce((acc, doc) => {
+                    const pathParts = doc.path.split('/');
+                    const category = pathParts[1] || 'Uncategorized';
+                    if (!acc[category]) {
+                        acc[category] = [];
+                    }
+                    acc[category].push(doc);
+                    return acc;
+                }, {});
+            }
+            setSortedDocs(sortDocs(parsedDocs));
+        };
 
-	if (!docs) {
-		return 'Loading...';
-	}
-
-	return (
-		<nav className={styles.nav}>
-			<div className={styles.header}>
-				<h1 className={styles.heading}>{params.docs[1].toUpperCase()}</h1>
-				<hr className={styles.headerDivider}></hr>
-				<p className={styles.category}>Docs</p>
-				<div
-					className={styles.toggle}
-					onClick={() => {
-						if (theme == 'dark') {
-							setTheme('light');
-						} else {
-							setTheme('dark');
-						}
-					}}
-				>
-					<i className={`${styles.toggleIcon} icon-moon ${theme == 'dark' ? styles.active : null}`}></i>
-					<i
-						className={`${styles.toggleIcon} icon-brightness ${theme == 'light' ? styles.active : null}`}
-					></i>
-				</div>
-			</div>
-			<div className={styles.search}>
-				<input type='string' className={styles.searchInput} placeholder='Search Docs...'></input>
-			</div>
-			<div className={styles.linksWrapper}>
-				{Object.entries(sortedDocs).map(([category, docs]) => (
-					<div
-						key={category}
-						className={`${styles.linksContainer} ${open.includes(category) ? styles.active : null}`}
-					>
-						<div
-							className={styles.mainLink}
-							onClick={() => {
-								if (open.includes(category)) {
-									setOpen(open.filter((item) => item !== category));
-								} else {
-									setOpen([...open, category]);
-								}
-							}}
-						>
-							<p className={styles.mainLinkText}>{category}</p>
-							<i className={`${styles.linkIcon} icon-moon`}></i>
-						</div>
-						<ul className={styles.links}>
-							{docs.map((doc, index) => (
-								<Link
-									key={index}
-									className={`${styles.link} ${activeDoc == doc.path ? styles.active : null}`}
-									href={`/${doc.path}`}
-								>
-									<i className={`${styles.linkIcon} icon-moon`}></i>
-									<p className={styles.linkText}>{doc.path.split('/')[2]}</p>
-								</Link>
-							))}
-						</ul>
-					</div>
-				))}
-			</div>
-			<div className={styles.support}>
-				<div className={styles.supportLink}>
-					<i className={`${styles.linkIcon} icon-moon`}></i>
-					<p className={styles.linkText}>Sign up</p>
-				</div>
-				<div className={styles.supportLink}>
-					<i className={`${styles.linkIcon} icon-moon`}></i>
-					<p className={styles.linkText}>Contact Us</p>
-				</div>
-			</div>
-		</nav>
-	);
+        fetchDocs();
+    }, []);
+    return (
+        <nav className={`${styles.nav} ${styles[theme]}`}>
+            <div className={styles.header}>
+                <h1 className={styles.heading} onClick={() => window.location.href(``)}>
+                    {project.charAt(0).toUpperCase() + project.slice(1)}
+                </h1>
+                <hr className={styles.headerDivider}></hr>
+                <a className={styles.category} href='/'>
+                    Docs
+                </a>
+                <div
+                    className={styles.toggle}
+                    onClick={() => {
+                        if (theme == 'dark') {
+                            setTheme('light');
+                        } else {
+                            setTheme('dark');
+                        }
+                    }}
+                >
+                    <i className={`${styles.toggleIcon} icon-moon ${theme == 'dark' ? styles.active : null}`}></i>
+                    <i
+                        className={`${styles.toggleIcon} icon-brightness ${theme == 'light' ? styles.active : null}`}
+                    ></i>
+                </div>
+            </div>
+            <div className={styles.search}>
+                <input type='string' className={styles.searchInput} placeholder='Search Docs...'></input>
+            </div>
+            <div className={styles.linksWrapper}>
+                {Object.entries(sortedDocs).map(([category, docs]) => (
+                    <div
+                        key={category}
+                        className={`${styles.linksContainer} ${open.includes(category) ? styles.active : null}`}
+                    >
+                        <div
+                            className={styles.mainLink}
+                            onClick={() => {
+                                if (open.includes(category)) {
+                                    setOpen(open.filter((item) => item !== category));
+                                } else {
+                                    setOpen([...open, category]);
+                                }
+                            }}
+                        >
+                            <p className={styles.mainLinkText}>{category}</p>
+                            <i className={`${styles.linkIcon} icon-angle-up`}></i>
+                        </div>
+                        <ul className={styles.links}>
+                            {docs.map((doc, index) => (
+                                <Link
+                                    key={index}
+                                    className={`${styles.link} ${activeDoc == doc.path ? styles.active : null}`}
+                                    href={`/${doc.path}`}
+                                >
+                                    <i className={`${styles.linkIcon} icon-moon`}></i>
+                                    <p className={styles.linkText}>{doc.path.split('/')[2]}</p>
+                                </Link>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.support}>
+                <div className={styles.supportLink}>
+                    <i className={`${styles.linkIcon} icon-moon`}></i>
+                    <p className={styles.linkText}>Sign up</p>
+                </div>
+                <div className={styles.supportLink}>
+                    <i className={`${styles.linkIcon} icon-envelope`}></i>
+                    <p className={styles.linkText}>Contact Us</p>
+                </div>
+            </div>
+        </nav>
+    );
 }
