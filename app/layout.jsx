42c7488550161d9f29481621fe67593727e8c3d8
Aertic <63727339+Aertic@users.noreply.github.com>
2024-10-18 21:08:46 +0200

    Wrap code in ThemeProvider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,12 +1,13 @@
 import '@/src/css/global/global.scss';
 import '@/src/css/global/icons.css';
+import { ThemeProvider } from '@/components/themeProvider';
 
 export default async function RootLayout({ children }) {
-	return (
-		<html lang='en'>
-			<body className='body' id='body'>
-				{children}
-			</body>
-		</html>
-	);
+    return (
+        <html lang='en'>
+            <body className='body' id='body'>
+                <ThemeProvider>{children}</ThemeProvider>
+            </body>
+        </html>
+    );
 }
